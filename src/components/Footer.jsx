f49434f5edc3ef5433d4d3c0f12a84ad7e0f9eba
Prasad Bachhav <pkbachhav1@gmail.com>
2025-09-23 00:50:34 +0530

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 text-center py-4 mt-8">
-      <p>© {new Date().getFullYear()} Atharv's AI Tools. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Prasad's AI Tools. All rights reserved.</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <h1 className="text-xl font-bold text-indigo-400"><Link to="/">AI Toolbox</Link></h1>
         <nav className="space-x-6">
           <Link to="/" className="hover:text-indigo-400">Home</Link>
-          <Link to="https://github.com/atharvviit26/PromptPilot/tree/main" className="hover:text-indigo-400">Get Code</Link>
+          <Link to="https://github.com/Prasad9294/PromptPilot" className="hover:text-indigo-400">Get Code</Link>
           {/* <Link to="/image-generator" className="hover:text-indigo-400">Image Generator</Link> */}
           {/* <Link to="/chatbot" className="hover:text-indigo-400">Chat Bot</Link> */}
         </nav>
